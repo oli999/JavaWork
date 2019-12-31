c0573e120615e1218d8dabd952b10bca02b788e4
kimgura <oli999@naver.com>
2019-12-31 10:39:50 +0900

    전화거는 기능 완성됨

--- Step01_Hello/src/test/main/MainClass01.java
+++ Step01_Hello/src/test/main/MainClass01.java
@@ -1,6 +1,7 @@
 package test.main;
 
 import test.mypac.Car;
+import test.mypac.Phone;
 
 public class MainClass01 {
 	public static void main(String[] args) {
@@ -8,5 +9,11 @@ public class MainClass01 {
 		System.out.println("원숭이");
 		Car car1=new Car();
 		car1.drive();
+		Phone p1=new Phone();
+		p1.call();
 	}
 }
+
+
+
+
---
+++ Step01_Hello/src/test/mypac/Phone.java
@@ -0,0 +1,7 @@
+package test.mypac;
+
+public class Phone {
+	public void call() {
+		System.out.println("전화를 걸어요");
+	}
+}
