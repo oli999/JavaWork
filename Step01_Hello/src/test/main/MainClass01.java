1d84beec204eb5f586cc2ddd89f2dbe9e015eaa7
kimgura <oli999@naver.com>
2019-12-31 10:18:51 +0900

    Car 클래스를 만들고 달렸어요

--- Step01_Hello/src/test/main/MainClass01.java
+++ Step01_Hello/src/test/main/MainClass01.java
@@ -1,8 +1,12 @@
 package test.main;
 
+import test.mypac.Car;
+
 public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("김구라");
 		System.out.println("원숭이");
+		Car car1=new Car();
+		car1.drive();
 	}
 }
---
+++ Step01_Hello/src/test/mypac/Car.java
@@ -0,0 +1,7 @@
+package test.mypac;
+
+public class Car {
+	public void drive() {
+		System.out.println("달려요");
+	}
+}
